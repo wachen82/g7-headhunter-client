263acc67e804fa362d4e1cf419f5714293bb7861
Greg Wachenek <greg.wachenek@gmail.com>
2023-04-21 07:15:41 +0100

    fix: change from default exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
+import React from 'react'
 
-
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+export const App = () => {
+    return <div></div>
 }
-
-export default App;
