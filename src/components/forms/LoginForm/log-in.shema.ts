c0a6a9e4e6a271cba9d0073626dec1b73008381e
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-04-25 08:08:01 +0200

    feat(LogIn): create Login component and handle errors

---
+++ src/components/forms/LoginForm/log-in.shema.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup'
+
+export const logInSchema = yup.object().shape({
+    email: yup
+        .string()
+        .required('Email jest wymagany')
+        .email('Podaj prawidłowy adres e-mail'),
+    password: yup
+        .string()
+        .required('Hasło jest wymagane')
+        .min(8, 'Hasło musi mieć co najmniej 8 znaków'),
+})
