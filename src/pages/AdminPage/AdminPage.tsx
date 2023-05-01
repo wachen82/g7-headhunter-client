24689bf0f5ae740539d21cf693a258360b7c8c1e
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-02 00:00:47 +0100

    feat(AdminPage): added form to import hr and reduced headline font

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,17 +1,18 @@
 import {Container, Divider, Typography} from "@mui/material";
 import {DropAndClickBox} from "../../components/admin/DropAndClickBox";
 import theme from "../../theme";
+import {HrForm} from "../../components/forms/HrForm/HrForm";
 
 export const AdminPage = () => {
     return (
         <main style={{display: 'flex', justifyContent: 'center'}}>
             <Container sx={{maxWidth: '80%', margin: '0 auto', textAlign: 'center', backgroundColor: theme.palette.secondary.light }}>
-                <Typography variant="h1" sx={{ fontSize: 48,  padding: 5}}>Panel administratora</Typography>
+                <Typography variant="h1" color={theme.palette.text.primary} sx={{ fontSize: 32,  padding: 3, textAlign:"left"}}>Panel administratora</Typography>
                 <Divider sx={{backgroundColor: theme.palette.primary.main}}/>
-                <Typography variant="h2" sx={{ fontSize: 32,  padding: 5}}>Importuj kursantów</Typography>
+                <Typography variant="h2" color={theme.palette.text.primary} sx={{ fontSize: 26,  padding: 5, textAlign:"center"}}>Importuj kursantów</Typography>
                 <DropAndClickBox />
-                <Typography variant="h2" sx={{ fontSize: 32,  padding: 5 }}>Dodaj headhunterów</Typography>
-                <Typography variant="body1">Formularz do dodawania headhunterów</Typography>
+                <Typography variant="h2" color={theme.palette.text.primary} sx={{ fontSize: 26,  padding: 5, textAlign:"center"}}>Dodaj headhunterów</Typography>
+                <HrForm/>
             </Container>
         </main>
     )
