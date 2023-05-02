33ec3dd84ae69a4404d341005ae25192c39e5a9a
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-05-02 21:13:37 +0200

    ref(forms): refactor login scheme and apply it to LoginForm component

--- src/components/forms/LoginForm/log-in.shema.ts
+++ src/components/forms/LoginForm/log-in.shema.ts
@@ -5,4 +5,5 @@ export const logInSchema = yup.object().shape({
         .string()
         .required('Email jest wymagany')
         .email('Podaj prawidłowy adres e-mail'),
+    password: yup.string().required('Hasło jest wymagane'),
 })
