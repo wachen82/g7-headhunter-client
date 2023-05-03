c981257c96e24aee0b90dbde04adc35bea7aaa3c
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-03 05:43:22 +0100

    fix(ErrorList): modified component for dynamic display of errors returned from BE

--- src/components/admin/ErrorList.tsx
+++ src/components/admin/ErrorList.tsx
@@ -1,96 +1,90 @@
-import React from "react";
 import {List, ListItem, ListItemText} from "@mui/material";
-
-interface RowData {
-    email: string;
-    courseCompletion: number;
-    courseEngagement: number;
-    projectDegree: number;
-    teamProjectDegree: number;
-    bonusProjectUrls: string;
+import theme from "../../theme";
+interface ErrorWithField extends ErrorCsv {
+    field: string;
 }
 
-export interface ErrorData {
-    row: RowData;
-    errors: {
-        [key: string]: {
-            row: number;
-            field: any;
-            error: boolean;
-            message: string;
-        }[];
-    };
+type ErrorOrErrorWithField = ErrorCsv | ErrorWithField;
+
+export interface ErrorCsv {
+    email?: { row: number; message: string };
+    courseCompletion?: { row: number; message: string };
+    courseEngagement?: { row: number; message: string };
+    projectDegree?: { row: number; message: string };
+    teamProjectDegree?: { row: number; message: string };
+    bonusProjectUrls?: Array<{ row: number; field: string; message: string }>;
 }
 
-const errors: ErrorData[] = [
-    {
-        row: {
-            email: "milena@gmail.com",
-            courseCompletion: 5,
-            courseEngagement: 5,
-            projectDegree: 5,
-            teamProjectDegree: 5,
-            bonusProjectUrls:
-                "https:/example.com/link1; https://example.com/link2; https://example.com/link3",
-        },
-        errors: {
-            bonusProjectUrls: [
-                {
-                    row: 1,
-                    field: "https:/example.com/link1",
-                    error: true,
-                    message: "błędny adres url",
-                },
-            ],
-        },
-    },
-    {
-        row: {
-            email: "milena@gmail.com",
-            courseCompletion: 2,
-            courseEngagement: 22,
-            projectDegree: 2,
-            teamProjectDegree: 4,
-            bonusProjectUrls:
-                "https://example.com/link1;https://example.com/link2; https://example.com/link3",
-        },
-        errors: {
-            courseEngagement: [
-                {
-                    row: 2,
-                    field: "22",
-                    error: true,
-                    message: "zła wartosc",
-                },
-            ],
-        },
-    },
-];
 
-export const ErrorList: React.FC = () => {
+interface ErrorListProps {
+    errors: ErrorOrErrorWithField[];
+}
+
+export const ErrorList = ({errors}: ErrorListProps) => {
+    const hasHeaderError = errors.some((error) => 'field' in error);
+
     return (
-        <List>
-            {errors.map((error, index) => (
-                <ListItem key={index}>
-                    <ListItemText
-                        primary={`Rząd ${index + 1}: ${error.errors ? Object.keys(error.errors).join(", ") : ""}`}
-                        secondary={
-                            <React.Fragment>
-                                {Object.entries(error.errors).map(([key, value], index) => (
-                                    <React.Fragment key={index}>
-                                        {value.map((errorItem) => (
-                                            <div key={errorItem.row}>
-                                                <span>{`${key} - Rząd ${errorItem.row}: `}</span>
-                                                <span>{`${errorItem.message}`}</span>
-                                            </div>
+        <>
+            {hasHeaderError && (
+                <p style={{color: theme.palette.error.main}}>
+                    Błędny nagłówek pliku CSV
+                </p>
+            )}
+            <List>
+                {errors.map((error, index) => {
+                    const errorValues = Object.values(error);
+                    const hasErrors = errorValues.some((error) => error !== undefined);
+                    if (!hasErrors) {
+                        return null;
+                    }
+                    return (
+                        <ListItem key={index}>
+                            <ListItemText
+                                primary={
+                                    <ul style={{color: theme.palette.primary.main, listStyle: "none"}}>
+                                        {`Row ${Object.values(error)[0].row}: `}
+                                    </ul>
+                                }
+                                secondary={
+                                    <>
+                                        {error.email?.message && (
+                                            <li style={{color: theme.palette.secondary.contrastText}}>
+                                                Email: {error.email.message}
+                                            </li>
+                                        )}
+                                        {error.courseCompletion?.message && (
+                                            <li style={{color: theme.palette.secondary.contrastText}}>
+                                                Course Completion: {error.courseCompletion.message}
+                                            </li>
+                                        )}
+                                        {error.courseEngagement?.message && (
+                                            <li style={{color: theme.palette.secondary.contrastText}}>
+                                                Course Engagement: {error.courseEngagement.message}
+                                            </li>
+                                        )}
+                                        {error.projectDegree?.message && (
+                                            <li style={{color: theme.palette.secondary.contrastText}}>
+                                                Project Degree: {error.projectDegree.message}
+                                            </li>
+                                        )}
+                                        {error.teamProjectDegree?.message && (
+                                            <li style={{color: theme.palette.secondary.contrastText}}>
+                                                Team Project Degree: {error.teamProjectDegree.message}
+                                            </li>
+                                        )}
+                                        {error.bonusProjectUrls?.map((bonusError) => (
+                                            <li style={{color: theme.palette.secondary.contrastText}}
+                                                key={bonusError.field}>
+                                                Bonus Project Url: {bonusError.field} - {bonusError.message}
+                                            </li>
                                         ))}
-                                    </React.Fragment>
-                                ))}
-                            </React.Fragment>
-                        }
-                    />
-                </ListItem>
-            ))}
-        </List>
+                                    </>
+                                }
+                            />
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </>
     );
 };
