efca35cb10e813e413a8963381cf145b089f9bd3
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-03 05:35:44 +0100

    feat(AdminPage): added styles for scrolling page

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -5,8 +5,8 @@ import {HrForm} from "../../components/forms/HrForm/HrForm";
 
 export const AdminPage = () => {
     return (
-        <main style={{display: 'flex', justifyContent: 'center'}}>
-            <Container sx={{maxWidth: '80%', margin: '0 auto', textAlign: 'center', backgroundColor: theme.palette.secondary.light }}>
+        <main style={{display: 'flex', flexDirection: "column", justifyContent: 'center'}}>
+            <Container sx={{maxWidth: '80%', margin: '0 auto', textAlign: 'center', backgroundColor: theme.palette.secondary.light, height: "100vh", overflowY: "auto" }}>
                 <Typography variant="h1" color={theme.palette.text.primary} sx={{ fontSize: 32,  padding: 3, textAlign:"left"}}>Panel administratora</Typography>
                 <Divider sx={{backgroundColor: theme.palette.primary.main}}/>
                 <Typography variant="h2" color={theme.palette.text.primary} sx={{ fontSize: 26,  padding: 5, textAlign:"center"}}>Importuj kursantów</Typography>
