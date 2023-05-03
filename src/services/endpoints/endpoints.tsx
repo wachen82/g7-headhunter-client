164432f2cd324df407830572e16c1128b72c2768
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-05-03 12:41:45 +0200

    ref(endpoints): change endpoints values

--- src/services/endpoints/endpoints.tsx
+++ src/services/endpoints/endpoints.tsx
@@ -2,6 +2,6 @@ export const ENDPOINTS = {
     signIn: '/auth/login',
     signOut: '/auth/logout',
     register: '/auth/register',
-    reset: '/auth/reset-password',
-    setNewPwd: '/auth/reset-password/{token}',
+    resetPassword: '/auth/reset-password',
+    updatePassword: '/auth/update-password',
 }
