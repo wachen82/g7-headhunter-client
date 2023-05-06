4acfacb7284e242821d1e1589e8a47b869819bab
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-06 19:04:45 +0100

    feat(HrPage1, HrPage2): added two page for hr view

---
+++ src/pages/HrPages/MenuView/HrPage1/HrPage1.tsx
@@ -0,0 +1,11 @@
+import { ButtonsAndInputView } from '../ButtonAndInputView/ButtonsAndInputView'
+import { MainPage1 } from './MainPage1'
+
+export const HrPage1 = () => {
+    return (
+        <>
+            <ButtonsAndInputView />
+            <MainPage1 />
+        </>
+    )
+}
---
+++ src/pages/HrPages/MenuView/HrPage2/HrPage2.tsx
@@ -0,0 +1,11 @@
+import { ButtonsAndInputView } from '../ButtonAndInputView/ButtonsAndInputView'
+import { MainPage2 } from './MainPage2'
+
+export const HrPage2 = () => {
+    return (
+        <>
+            <ButtonsAndInputView />
+            <MainPage2 />
+        </>
+    )
+}
