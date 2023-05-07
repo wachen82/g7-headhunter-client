2282bb6b6b614979990e98be373e332583537aa9
Jakub Markiewicz <markiewicz267@gmail.com>
2023-05-07 22:47:25 +0200

    feature: add style and array method for portfolio links

--- src/pages/StudentPage/StudentPage.tsx
+++ src/pages/StudentPage/StudentPage.tsx
@@ -8,6 +8,7 @@ import { PortfolioBar } from '../../components/common/StudentPortfolio/Portfolio
 import { PortfolioContent } from '../../components/common/StudentPortfolio/PortfolioContent'
 import { GradesBox } from '../../components/common/StudentPortfolio/Grades/GradesBox'
 import { ExpectationsBox } from '../../components/common/StudentPortfolio/Expectations/ExpectationsBox'
+import { PortfolioLink } from '../../components/common/StudentPortfolio/PortfolioLink/PortfolioLink'
 
 export const StudentPage = () => {
     useTitle('MegaK HeadHunter - Kursant i jego CV')
@@ -22,6 +23,9 @@ export const StudentPage = () => {
         //grades - dodać cząstkowe oceny
         workExpectations,
         portfolioContent,
+        portfolioUrl,
+        scrumUrl,
+        finalProjectUrl,
     ] = [
         'https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/118124271_10224604112957191_6232217390682151742_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGZRrCSGbBKnDhFLPrmFDMTS0hGwM3YatVLSEbAzdhq1b-F83hHow85cjsPBJIU6GQ&_nc_ohc=LqtPhPwTXoIAX95j5tZ&_nc_ht=scontent-vie1-1.xx&oh=00_AfCPE96i_mxSjn_VTfNdXHh1QkTiVAsn-2h1PlfuPOCM-A&oe=647CDCDE',
         'Jakub Markiewicz',
@@ -38,6 +42,15 @@ export const StudentPage = () => {
             experience: 6,
         },
         'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        ['https://Loremipsum/dolor/sit/amet'],
+        [
+            'https://github.com/Ami777/MegaKursTest/commits?author=Ami777',
+            'https://github.com/Ami777/MegaKursTest/pulls?q=is%3Apr+reviewed-by%3AAmi777',
+        ],
+        [
+            'https://Loremipsum/dolor/sit/amet',
+            'https://Loremipsum/dolor/sit/amet',
+        ],
     ]
 
     const {
@@ -60,6 +73,7 @@ export const StudentPage = () => {
             <Box sx={{ width: '100%' }}>
                 <UsersAppBar avatarUrl={avatarUrl} userName={userName} />
             </Box>
+
             <Box
                 sx={{
                     display: 'flex',
@@ -85,6 +99,7 @@ export const StudentPage = () => {
                             about={about}
                         />
                     </Box>
+
                     <Box
                         sx={{
                             display: 'flex',
@@ -93,6 +108,7 @@ export const StudentPage = () => {
                     >
                         <PortfolioBar sectionName={'Oceny'} />
                         <GradesBox />
+
                         <PortfolioBar
                             sectionName={
                                 'Oczekiwanie w stosunku do zatrudnienia'
@@ -109,15 +125,62 @@ export const StudentPage = () => {
 
                         <PortfolioBar sectionName={'Edukacja'} />
                         <PortfolioContent content={portfolioContent} />
+
                         <PortfolioBar sectionName={'Kursy'} />
                         <PortfolioContent content={portfolioContent} />
+
                         <PortfolioBar sectionName={'Doświadczenie zawodowe'} />
                         <PortfolioContent content={portfolioContent} />
+
                         <PortfolioBar sectionName={'Portfolio'} />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                backgroundColor: '#222324',
+                                width: '1176px',
+                                padding: '17px 26px 15px 26px',
+                                margin: '0 0 12px 4px',
+                            }}
+                        >
+                            {portfolioUrl.map((link) => (
+                                <PortfolioLink url={link} />
+                            ))}
+                        </Box>
+
                         <PortfolioBar
                             sectionName={'Projekt w zespole Scrumowym'}
                         />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                backgroundColor: '#222324',
+                                width: '1176px',
+                                padding: '17px 26px 15px 26px',
+                                margin: '0 0 12px 4px',
+                            }}
+                        >
+                            {scrumUrl.map((link) => (
+                                <PortfolioLink url={link} />
+                            ))}
+                        </Box>
+
                         <PortfolioBar sectionName={'Projekt na zaliczenie'} />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                backgroundColor: '#222324',
+                                width: '1176px',
+                                padding: '17px 26px 15px 26px',
+                                margin: '0 0 12px 4px',
+                            }}
+                        >
+                            {finalProjectUrl.map((link) => (
+                                <PortfolioLink url={link} />
+                            ))}
+                        </Box>
                     </Box>
                 </Box>
             </Box>
