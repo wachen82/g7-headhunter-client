ae2e8d3a760dc0c1fb541778c5ba3b16da88101b
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-05-10 21:18:54 +0200

    ref(LoginForm): remove redundant snackbars and add ;

--- src/components/forms/LoginForm/LoginForm.tsx
+++ src/components/forms/LoginForm/LoginForm.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
-import { Box } from '@mui/material'
-import { logInSchema } from './log-in.shema'
-import axios, { isAxiosError } from 'axios'
-import { FormValues, SnackBarEnum } from '../../../types/formValues'
-import { apiUrl } from '../../../config/api'
-import { ENDPOINTS } from '../../../services/endpoints/endpoints'
-import { CustomSnackBar } from '../../common/CustomSnackBar/CustomSnackBar'
-import { CustomBasicForm } from '../../common/CustomBasicForm/CustomBasicForm'
-import { useSnackBar } from '../../../hooks/useSnackBar'
-import { loginDataArr } from './login-data'
-import { useAppDispatch } from '../../../hooks/reduxHooks'
-import { setLogin } from '../../../state/authSlice'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { Box } from '@mui/material';
+import { logInSchema } from './log-in.shema';
+import axios, { isAxiosError } from 'axios';
+import { FormValues } from '../../../types/formValues';
+import { apiUrl } from '../../../config/api';
+import { ENDPOINTS } from '../../../services/endpoints/endpoints';
+import { CustomSnackBar } from '../../common/CustomSnackBar/CustomSnackBar';
+import { CustomBasicForm } from '../../common/CustomBasicForm/CustomBasicForm';
+import { useSnackBar } from '../../../hooks/useSnackBar';
+import { loginDataArr } from './login-data';
+import { useAppDispatch } from '../../../hooks/reduxHooks';
+import { setLogin } from '../../../state/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginForm = () => {
-    const dispatch = useAppDispatch()
-    const navigate = useNavigate()
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
     const {
         snackBarMessage,
         snackBarType,
@@ -47,44 +47,32 @@ export const LoginForm = () => {
                 method: 'POST',
                 data: data,
                 withCredentials: true,
-            })
-            dispatch(setLogin({ user: res.data }))
-            reset(defaultValues)
+            });
+            dispatch(setLogin({ user: res.data }));
+            reset(defaultValues);
 
             switch (res.data.role) {
                 case 'Admin':
-                    showSnackBar(
-                        'Zalogowałeś się poprawnie jako Admin',
-                        SnackBarEnum.SUCCESS_MESSAGE
-                    )
-                    navigate(`/admin/${res.data._id}`)
-                    break
+                    navigate(`/admin/${res.data._id}`);
+                    break;
                 case 'Kursant':
-                    showSnackBar(
-                        'Zalogowałeś się poprawnie jako Kursant',
-                        SnackBarEnum.SUCCESS_MESSAGE
-                    )
-                    navigate(`/user/${res.data._id}`)
+                    navigate(`/user/${res.data._id}`);
 
-                    break
+                    break;
                 case 'HR':
-                    showSnackBar(
-                        'Zalogowałeś się poprawnie jako HR',
-                        SnackBarEnum.SUCCESS_MESSAGE
-                    )
-                    navigate(`/hr/${res.data._id}`)
-                    break
+                    navigate(`/hr/${res.data._id}`);
+                    break;
                 default:
-                    showSnackBar('Błąd logowania, spróbuj ponownie.')
+                    showSnackBar('Błąd logowania, spróbuj ponownie.');
             }
         } catch (err: unknown) {
             if (isAxiosError(err)) {
                 if (err.response?.status === 403) {
                     showSnackBar(
                         'Logujesz się jako nieaktywny użytkownik. Czekaj na potwierdzenie przez administratora'
-                    )
+                    );
                 } else {
-                    showSnackBar('Taki użytkownik nie jest zarejestrowany')
+                    showSnackBar('Taki użytkownik nie jest zarejestrowany');
                 }
             } else {
                 showSnackBar('Wystąpił niespodziewany błąd');
