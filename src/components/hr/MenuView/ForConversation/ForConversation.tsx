0929eb97344795e663a4709e3924a5c1c54c119f
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-10 14:15:32 +0100

    fix(ForConversation): deleted unnecessary directory

--- src/components/hr/MenuView/ForConversation/ForConversation.tsx
+++ src/components/hr/MenuView/ForConversation/ForConversation.tsx
@@ -1,11 +1,18 @@
-import { ButtonsAndInputView } from '../ButtonAndInputView/ButtonsAndInputView'
-import { ForConversationMain } from './ForConversationMain'
+import { ButtonsAndInputView } from '../ButtonAndInputView/ButtonsAndInputView';
+import { CustomAccordionFilter } from '../../../common/Accordion/CustomAccordionFilter';
+import { users } from '../AvailableStudents/AvailableStudents';
+import { Container, Divider } from '@mui/material';
+import theme from '../../../../theme';
+import React from 'react';
 
 export const ForConversation = () => {
     return (
         <>
             <ButtonsAndInputView />
-            <ForConversationMain />
+            <Container>
+                <Divider sx={{ bgcolor: theme.palette.grey['900'] }} />
+                <CustomAccordionFilter users={users} />
+            </Container>
         </>
-    )
-}
+    );
+};
