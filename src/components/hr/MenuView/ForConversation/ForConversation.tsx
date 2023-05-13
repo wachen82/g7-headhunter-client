89e8838eab614eb772a4e2c17519e3937424906d
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-13 04:00:54 +0100

    fix(ForConversation): deleted users props

--- src/components/hr/MenuView/ForConversation/ForConversation.tsx
+++ src/components/hr/MenuView/ForConversation/ForConversation.tsx
@@ -2,17 +2,16 @@ import { ButtonsAndInputView } from '../ButtonAndInputView/ButtonsAndInputView';
 import { CustomAccordionFilter } from '../../../common/Accordion/CustomAccordionFilter';
 import { Container, Divider } from '@mui/material';
 import theme from '../../../../theme';
-import { UserListProps } from '../../../common/Accordion/CustomAccordion';
 
 
-export const ForConversation = ({users}:UserListProps) => {
+export const ForConversation = () => {
 
     return (
         <>
             <ButtonsAndInputView />
             <Container>
                 <Divider sx={{ bgcolor: theme.palette.grey['900'] }} />
-                <CustomAccordionFilter users={users} />
+                <CustomAccordionFilter />
             </Container>
         </>
     );
