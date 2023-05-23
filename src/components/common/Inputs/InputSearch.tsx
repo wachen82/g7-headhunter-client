256d45fd9e343adf83985997f8086971bb48e18f
Milena Pieńkosz <designbymilencja@gmail.com>
2023-05-23 04:07:13 +0200

    feat(InputSearch): added handling of search input and context

--- src/components/common/Inputs/InputSearch.tsx
+++ src/components/common/Inputs/InputSearch.tsx
@@ -1,36 +1,52 @@
-import { InputAdornment, TextField } from '@mui/material'
-import { Search } from '@mui/icons-material'
-import theme from '../../../theme'
+import { InputAdornment, TextField } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import theme from '../../../theme';
+import React, { useContext, useState } from 'react';
 
-interface InputProps {
-    onClick?: () => void
-}
+import { SearchValueContext } from '../../../context/SearchValueContext';
 
-export const InputSearch = ({ onClick }: InputProps) => {
-    return (
-        <TextField
-            onClick={onClick}
-            variant="outlined"
-            size="small"
-            placeholder="Szukaj"
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <Search sx={{ color: theme.palette.grey['300'] }} />
-                    </InputAdornment>
-                ),
-                sx: {
-                    textTransform: 'none',
-                    width: '365px',
-                    height: '38px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-start',
-                    fontWeight: 'normal',
-                    color: theme.palette.grey['300'],
-                    bgcolor: theme.palette.grey['800'],
-                },
-            }}
-        />
-    )
-}
+export const InputSearch = () => {
+    const { searchValue, setSearchValue } = useContext(SearchValueContext);
+    const [inputValue, setInputValue] = useState(searchValue);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleInputKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            setSearchValue(inputValue);
+            setInputValue('');
+        }
+    };
+
+    return (<>
+            <TextField
+                variant='outlined'
+                size='small'
+                placeholder='Szukaj'
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleInputKeyPress}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position='start'>
+                            <Search sx={{ color: theme.palette.grey['300'] }} />
+                        </InputAdornment>
+                    ),
+                    sx: {
+                        textTransform: 'none',
+                        width: '365px',
+                        height: '38px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start',
+                        fontWeight: 'normal',
+                        color: theme.palette.grey['300'],
+                        bgcolor: theme.palette.grey['800'],
+                    },
+                }}
+            />
+        </>
+    );
+};
