bd1af08ee799a3333c05851ce9680a59f5c0066f
Milena Pieńkosz <designbymilencja@gmail.com>
2023-06-12 20:47:44 +0200

    feat(types/status): added enum for status compare

---
+++ src/types/status.ts
@@ -0,0 +1,6 @@
+// @TODO: importowac ten enum z BE, na ten moment podczas kompilacji pojawia sie błą∂ uniemozliwiajacy import
+export enum Status {
+    available = 'Dostępny',
+    reserved = 'W trakcie rozmowy',
+    employed = 'Zatrudniony',
+}
